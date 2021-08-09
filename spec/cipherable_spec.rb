a79b772705e275791dd50b60709921da4d05a257
Ida Olson <idaolson@gmail.com>
2021-08-09 16:10:06 -0600

    Add test for random key generator

--- spec/cipherable_spec.rb
+++ spec/cipherable_spec.rb
@@ -11,19 +11,24 @@ RSpec.describe Cipherable do
     end
 
     it "can create an array of offsets" do
-      expect(enigma.calc_offset('040895')).to eq([1, 0, 2, 5])
+      expect(enigma.calc_offset("040895")).to eq([1, 0, 2, 5])
     end
 
     it "can create an array of shifts" do
       expect(enigma.shift("02715", "040895")).to eq([3, 27, 73, 20])
     end
 
+    it "can shift letters using cipher, ignore symbols, and return new string" do
+      expect(enigma.letter_index("hello world!", [3, 27, 73, 20])).to eq("keder ohulw!")
+    end
+
     it "can generate today's date" do
       expect(enigma.today).to eq("090821")
     end
 
-    it "can shift letters using cipher, ignore symbols, and return new string" do
-      expect(enigma.letter_index("hello world!", [3, 27, 73, 20])).to eq("keder ohulw!")
+    it "can generate a random number string of 5 digits with 0s padding" do
+      expect(random_key.length).to eq(5)
+      expect(random_key.class).to eq(String)
     end
   end
 end
