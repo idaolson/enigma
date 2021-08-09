5ce8e5f512fe229e1b0fa491c210d68b2cee46d5
Ida Olson <idaolson@gmail.com>
2021-08-09 16:03:06 -0600

    Read from messages.txt and write to encrypted.txt & change file name

---
+++ lib/encrypt.rb
@@ -0,0 +1,27 @@
+require './lib/enigma'
+
+# ARGV == ["message.txt", "encrypted.txt"]
+# ARGV[0] == "message.txt"
+# ARGV[1] == "encrypted.txt"
+# ARGV[2] == key
+# ARGV[3] == date
+enigma = Enigma.new
+
+message_file = ARGV[0]
+encrypted_file = ARGV[1]
+message = File.open(message_file, "r")
+
+# if ARGV[2].nil? && ARGV[3].nil?
+#   encrypt = enigma.encrypt(message.read)
+# elsif !ARGV[2].nil? && ARGV[3].nil?
+#   encrypt = enigma.encrypt(message.read, ARGV[2])
+# elsif !ARGV[2].nil? && !ARGV[3].nil?
+#   encrypt = enigma.encrypt(message.read, ARGV[2], ARGV[3])
+# end
+
+encrypt = enigma.encrypt(message.read)
+
+
+File.new(encrypted_file, 'w').write(encrypt[:encryption])
+
+puts "Created #{encrypted_file} with the key #{encrypt[:key]} and date #{encrypt[:date]}"
--- lib/encrypted.rb
+++
@@ -1,5 +0,0 @@
-ARGV == ["message.txt", "encrypted.txt"]
-ARGV[0] == "message.txt"
-ARGV[1] == "encrypted.txt"
-
-handle = File.open(ARGV[0], "r")
