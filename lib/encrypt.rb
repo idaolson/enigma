1369b89c5b7b18794551b5484820d26216434b05
Ida Olson <idaolson@gmail.com>
2021-08-09 16:13:20 -0600

    Add if/else conditional to accept a key and date from user

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -11,16 +11,13 @@ message_file = ARGV[0]
 encrypted_file = ARGV[1]
 message = File.open(message_file, "r")
 
-# if ARGV[2].nil? && ARGV[3].nil?
-#   encrypt = enigma.encrypt(message.read)
-# elsif !ARGV[2].nil? && ARGV[3].nil?
-#   encrypt = enigma.encrypt(message.read, ARGV[2])
-# elsif !ARGV[2].nil? && !ARGV[3].nil?
-#   encrypt = enigma.encrypt(message.read, ARGV[2], ARGV[3])
-# end
-
-encrypt = enigma.encrypt(message.read)
-
+if ARGV[2].nil? && ARGV[3].nil?
+  encrypt = enigma.encrypt(message.read)
+elsif !ARGV[2].nil? && ARGV[3].nil?
+  encrypt = enigma.encrypt(message.read, ARGV[2])
+elsif !ARGV[2].nil? && !ARGV[3].nil?
+  encrypt = enigma.encrypt(message.read, ARGV[2], ARGV[3])
+end
 
 File.new(encrypted_file, 'w').write(encrypt[:encryption])
 
