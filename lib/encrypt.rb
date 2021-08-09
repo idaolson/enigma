7dbb3b819452fd497c4ba107fa08b793a7d8fad1
Ida Olson <idaolson@gmail.com>
2021-08-09 17:34:25 -0600

    Refactor and add variables

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,22 +1,19 @@
 require './lib/enigma'
 
-# ARGV == ["message.txt", "encrypted.txt"]
-# ARGV[0] == "message.txt"
-# ARGV[1] == "encrypted.txt"
-# ARGV[2] == key
-# ARGV[3] == date
 enigma = Enigma.new
 
 message_file = ARGV[0]
 encrypted_file = ARGV[1]
+key = ARGV[2]
+date = ARGV[3]
 message = File.open(message_file, "r")
 
-if ARGV[2].nil? && ARGV[3].nil?
+if key.nil? && date.nil?
   encrypt = enigma.encrypt(message.read)
-elsif !ARGV[2].nil? && ARGV[3].nil?
-  encrypt = enigma.encrypt(message.read, ARGV[2])
-elsif !ARGV[2].nil? && !ARGV[3].nil?
-  encrypt = enigma.encrypt(message.read, ARGV[2], ARGV[3])
+elsif !key.nil? && date.nil?
+  encrypt = enigma.encrypt(message.read, key)
+elsif !key.nil? && !date.nil?
+  encrypt = enigma.encrypt(message.read, key, date)
 end
 
 File.new(encrypted_file, 'w').write(encrypt[:encryption])
