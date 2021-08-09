ca03ef7ae38bd9e891b64e87b2ef51b5a62f3cf1
Ida Olson <idaolson@gmail.com>
2021-08-09 16:10:29 -0600

    Add random key generator

--- lib/cipherable.rb
+++ lib/cipherable.rb
@@ -26,18 +26,23 @@ module Cipherable
   end
 
   def letter_index(message, shift_array)
-    message = message.downcase.split('')
+    message_array = message.downcase.split('')
     letter_array = []
-    message.length.times do |i|
-      if character_set.index(message[i]).nil?
-        letter_array << message[i]
-      else letter_array << character_set[(character_set.index(message[i]) + shift_array[i % 4]) % 27]
+    message_array.length.times do |i|
+      if character_set.index(message_array[i]).nil?
+        letter_array << message_array[i]
+      else
+        letter_array << character_set[(character_set.index(message_array[i]) + shift_array[i % 4]) % 27]
       end
     end
     letter_array.join
   end
 
   def today
-    Date.today.strftime('%d%m%y')
+   Date.today.strftime('%d%m%y')
+  end
+
+  def random_key
+   Random.rand(99999).to_s.rjust(5, '0')
   end
 end
